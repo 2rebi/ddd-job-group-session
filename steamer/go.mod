e6fb9a7d27647c94acf1fd14b7e25f5654f0fc02
leejaeseong <fg9658@gmail.com>
2019-11-09 13:08:05 +0900

    빈칸 추가

--- steamer/go.mod
+++ steamer/go.mod
@@ -1,3 +1,5 @@
 module steamer
 
 go 1.12
+
+require github.com/nareix/joy4 v0.0.0-20181022032202-3ddbc8f9d431
--- steamer/main.go
+++ steamer/main.go
@@ -1,5 +1,80 @@
 package main
 
-func main () {
-	print("hello world")
+import (
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"sync"
+
+	"github.com/nareix/joy4/av/pubsub"
+	"github.com/nareix/joy4/format/rtmp"
+)
+
+var (
+	addr = flag.String("l", ":8089", "host:port of the go-rtmp-server")
+)
+
+type writeFlusher struct {
+	httpflusher http.Flusher
+	io.Writer
+}
+
+func (self writeFlusher) Flush() error {
+	self.httpflusher.Flush()
+	return nil
+}
+
+func main() {
+	flag.Parse()
+	server := &rtmp.Server{}
+
+	l := &sync.RWMutex{}
+	type Channel struct {
+		que *pubsub.Queue
+	}
+	channels := map[string]*Channel{}
+
+	server.HandlePublish = func(conn *rtmp.Conn) {
+		/*streams, _ := */conn.Streams()
+
+		l.Lock()
+		path := strings.Split(conn.URL.Path, "/")
+		if len(path) != 2 {
+			fmt.Println("wrong path->", conn.URL.RequestURI())
+			return
+		}
+
+		_ := path[1]
+		fmt.Println("request string->", conn.URL.RequestURI())
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		path := strings.Split(r.URL.Path, "/")
+		if len(path) == 3 && path[2] == "stream" {
+			l.RLock()
+			ch := channels[path[1]]
+			l.RUnlock()
+			if ch != nil {
+			}
+		} else {
+			fmt.Println("Request url: ", r.URL.Path)
+			content, err := ioutil.ReadFile("index.html")
+			if err != nil {
+				w.WriteHeader(404)
+				w.Write([]byte(http.StatusText(404)))
+				return
+			}
+			w.Header().Add("Content-Type", "text/html")
+			w.Write(content)
+		}
+
+	})
+
+	go http.ListenAndServe(*addr, nil)
+	fmt.Println("Listen and serve ", *addr)
+
+	server.ListenAndServe()
 }
